d2802e5bed3c594326c66f6d2f61e3135df111a0
AungHtutHein20167 <55374185+AungHtutHein20167@users.noreply.github.com>
2019-09-17 11:15:54 +0630

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,26 @@
+{
+  "name": "hyperbeast",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Aaronkst/hyperbeast.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/Aaronkst/hyperbeast/issues"
+  },
+  "homepage": "https://github.com/Aaronkst/hyperbeast#readme",
+  "dependencies": {
+    "express": "^4.17.1",
+    "http": "0.0.0",
+    "requestify": "^0.2.5",
+    "viber-bot": "^1.0.14"
+  }
+}
---
+++ example code.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const express = require('express');
+const app = express();
+const requestify = require('requestify');
+const TextMessage = require('viber-bot').Message.Text;
+const UrlMessage = require('viber-bot').Message.Url;
+const ContactMessage = require('viber-bot').Message.Contact;
+const PictureMessage = require('viber-bot').Message.Picture;
+const VideoMessage = require('viber-bot').Message.Video;
+const LocationMessage = require('viber-bot').Message.Location;
+const StickerMessage = require('viber-bot').Message.Sticker;
+const RichMediaMessage = require('viber-bot').Message.RichMedia;
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
+
